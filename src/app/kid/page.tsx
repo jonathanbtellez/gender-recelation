5a540fc9ed995dd416a3c19fdf827fc1f2873c3c
jonathan Tellez <jtellez@itroi.co>
2025-04-24 11:27:39 -0500

    fix: solve aling issues

--- src/app/kid/page.tsx
+++ src/app/kid/page.tsx
@@ -31,20 +31,18 @@ export default function Kid() {
 
   return (
     <Grid
-      sx={{
-        width: '80vw',
-        height: '100vh',
-        margin: 'auto'
-      }}
-      display={'grid'}
-      alignItems={'center'}
-      justifyContent={'center'}
-      container
+    container
+    sx={{
+      width: '80%',
+      height: '100dvh',
+      margin: 'auto',
+    }}
+    alignItems="center"
+    justifyContent="center"
     >
 
-      <Grid size={12} justifyItems={'center'} >
-
-        <Grid size={12} height={'200px'}>
+<Grid size={12} height={'200px'}>
+<Grid size={12} height={'200px'} textAlign={'center'}>
           {isTextVisible &&
             (
               <Fade in={isTextVisible} timeout={1000}>
@@ -55,15 +53,19 @@ export default function Kid() {
             )}
         </Grid>
 
-        <motion.div
-          initial={{ opacity: 0, scale: 0 }}
-          animate={{ opacity: 1, scale: 2 }}
-          transition={{
-            duration: 9,
-            scale: { type: "spring", visualDuration: 5, bounce: 3 },
-          }}
-          style={ball}
-        />
+        <Grid size={12} display="flex"
+          alignItems="center"
+          justifyContent="center" >
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 2 }}
+            transition={{
+              duration: 9,
+              scale: { type: "spring", visualDuration: 5, bounce: 3 },
+            }}
+            style={ball}
+          />
+        </Grid>
       </Grid>
     </Grid>
   );
